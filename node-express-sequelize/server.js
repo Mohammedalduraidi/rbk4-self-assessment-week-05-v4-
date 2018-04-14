806d2201b00b13718c47f0b3815250e1e656aa94
Mohammedalduraidi <Mohd.alduraidi@gmail.com>
2018-04-14 10:00:15 +0300

    self-assessment-week5-v4

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
 
@@ -9,7 +10,32 @@ var User = sequelize.define('User', {
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
+app.get('/users', function (req, res) {
+  // i need to fetch the username form Users table
 
+})
+
+
+
+app.get('/', function (req, res) {
+//.fetch().then()!!
+
+// 	var query = "SELECT * FROM Users";
+ 
+ 
+// User(query, function(err, user) {
+    
+//     if (err) {
+        
+//         res.status(404)
+//         res.send(err)
+         
+//     }
+    
+//      res.send(user)       
+// });
+
+})
 
 
 module.exports = { 
